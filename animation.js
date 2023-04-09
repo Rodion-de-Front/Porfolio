6490a9490e4b56c4f25e0e0d815205029adf19d1
Rodion <116435779+Rodion-de-Front@users.noreply.github.com>
2023-04-09 19:43:25 +0300

    Delete animation.js

--- animation.js
+++
@@ -1,8 +0,0 @@
-function transfer(date) {
-    document.querySelector('main').innerHTML = null;
-    document.querySelector('main').append(document.getElementById(date).content.cloneNode(true));
-}
-
-function sendMessage() {
-    document.getElementById('send').innerHTML = `<h1 class='thanks'>Спасибо за доверие. Скоро  свяжусь с Вами.<h1>`;
-}
